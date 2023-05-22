1c30c42f2d9135170a7c729ee42cb7d4ad62e93c
Muhammad ismaeel <muhammad.ismaeel5162@gmail.com>
2023-05-22 19:42:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import OurComunity from './components/OurComunity';
-import CallUs from './components/CallUs';
-import PlumbingHelp from './components/PlumbingHelp';
-import Experts from './components/Experts';
-import powers from '../src/assets/images/powers.png'
-import experts from '../src/assets/images/experts.png'
-import toilet from '../src/assets/images/toilet.png'
-import heatPump from '../src/assets/images/heatPump.png'
-import leakingPipes from '../src/assets/images/leakingPipes.png'
-import sewerLines from '../src/assets/images/sewerLines.png'
-import waterHeater from '../src/assets/images/waterHeater.png'
-import drainClean from '../src/assets/images/drainClean.png'
-import appliance from '../src/assets/images/appliance.png'
-import pipeRepair from '../src/assets/images/pipeRepair.png'
-import filter from '../src/assets/images/filter.png'
-import noritz from '../src/assets/images/noritz.png'
-import brandWhite from '../src/assets/images/brandWhite.png'
+import wildHoney from '../src/assets/images/wildHoney.png'
+import hairSalon1 from '../src/assets/images/hairSalon1.png'
 import icon1 from '../src/assets/images/icon1.svg'
 import icon2 from '../src/assets/images/icon2.svg'
 import icon3 from '../src/assets/images/icon3.svg'
 import icon4 from '../src/assets/images/icon4.svg'
-import fb from '../src/assets/images/fb.svg'
-import insta from '../src/assets/images/insta.svg'
-import linkdIn from '../src/assets/images/in.svg'
-import youTube from '../src/assets/images/youTube.svg'
-import twitter from '../src/assets/images/twitter.svg'
-import Footer from '../src/components/Footer'
+import icon5 from '../src/assets/images/icon5.svg'
+import callIcon from '../src/assets/images/callIcon.svg'
+
 function App() {
   const headerData = {
-    imageUrl: powers,
+    imageUrl: wildHoney,
     texts: [
       'About Us',
       'Services',
@@ -39,175 +20,44 @@ function App() {
       'FAQs',
       'Contact Us',
     ],
-  };
-
-  const heroData = {
-    title: "Get In Touch",
-    buttonText: "Contact Us",   
-  };
-  const expertsData = {
-    title: "Trust Us, We’re Experts",
-    discription: "Since 1914, Powers Plumbing has had a passion for performing high quality plumbing service.",   
-    discription2: "We have seen a lot of unique situations over these years. We are here to help with your next service or remodel project. Call us now to discuss what you want to accomplish. We will perform all of your plumbing repairs so that you will be in service for years to come. See our Resources page for more articles and information.", 
-    image: experts  ,
-    subSectionTitle:"The team at Powers Plumbing was great to work with.",
-    subSectionDiscrp:"They have great availability, fair pricing and do excellent work. I look forward to using them again when our next plumbing need arises.",
-    name:"Aaron Iffland"
-  };
-  
-  const ourComunityData = {
-    title: "Involved In Our Community",
-    discription: "Powers Plumbing has been deeply involved in the Mission Hills Community since the early part of the last century and we are here to stay. We are the longest running sponsor of the Presidio Little League. On April 5th, 2014, County Supervisor Ron Roberts declared Powers Plumbing Day throughout San Diego County and Mayor Kevin Faulconer declared August 1st, 2014 Powers Plumbing day in the City of San Diego in recognition of our 100th year serving San Diego. Thank you for your support and genuine affection towards our team. See our About Us page to learn more.",   
-  };
-  const plumbingData = {
-    title: "Plumbing Problem? We Can Help You!",
-    discription: "Below is a list of just some of the services we offer as well as appliances and equipment we work on. Whether it’s plumbing for a remodel, tankless water heater, sewer line camera inspection or toilet replacement parts, we take quality seriously. Our plumbers are great mechanics and our company strives to provide repair services and sources parts that will get you your money’s worth. To learn more, visit our Service & Repair and Remodel pages.",   
-    discription2:"We Use Only Best Brands",
-    image:noritz,
-    image2:brandWhite,
-  };
-
-  const callUsData = {
-    title: "Call Us Today",
-    discription: "We are here to help!",   
-    btnText:"Schedule Now! ",
-    image :powers ,
-    powerText:"Est.1914",
+    text: "Connect with us",
     icon1:icon1,
     icon2:icon2,
     icon3:icon3,
     icon4:icon4,
-    licence:"License #12345",
-    mail:"mike@marrowmerchant.com",
-    location:"6200 Irvine Blvd, Irvine, CA 92620",
-    phone:"(800) 555-4321",
-    servingSan:"Serving San Diego For Over 100 Years",
-    ServingSanDisc1:"Since 1914, Powers Plumbing has been on a mission to provide exceptional high quality plumbing services for the San Diego communities we serve. We are deeply connected to our historic Mission Hills community and are proud to serve multiple generations of families throughout San Diego.",
-    ServingSanDisc2:"We continue to strive to be the best plumbing shop in San Diego by living up to high ethical standards that have been passed down to us through generations of honorable service."
-
+    icon5:icon5,
+    callIcon:callIcon,
+    phone:"(619) 255-9779",
+    btnText:"Book Now",
+    title: "OUR SAN DIEGO HAIR SALON",
+    discription: "Wild Honey Salons in Bankers Hill, San Diego CA (formerly Fox and Jane) is the premier salon for beautiful haircuts, hair color, balayage, highlights, hair painting, color correction, scissor/razor cuts, and blowouts. Our stylists make this the most requested hair salon in the Bankers Hill and Hillcrest area.",   
+    discription2: "Eu velit sed nisl amet nisl morbi tristique feugiat. At ac ornare egestas pharetra egestas eu et ullamcorper. Sed eleifend vulputate vel faucibus volutpat.", 
+    image: hairSalon1  ,
+    discription3:"Nibh eget urna nullam at dolor lectus feugiat luctus. Tristique molestie sit tempor pulvinar. Lorem amet dignissim cras odio ut feugiat. Sit id nunc suspendisse viverra in neque. Vitae commodo convallis odio mattis ornare non felis.",
 
   };
-  const cards = [
-    {
-      id: 1,
-      image: toilet,
-      text: 'Toilets',
-    },
-    {
-      id: 2,
-      image: waterHeater,
-      text: 'Gas Water Heater',
-    },
-    {
-      id: 3,
-      image: heatPump,
-      text: 'Heat Pumps',
-    },
-    {
-      id: 4,
-      image: sewerLines,
-      text: 'Sewer Lines  ',
-    },
-    {
-      id: 5,
-      image: leakingPipes,
-      text: 'Leaking Pipes',
-    },
-    {
-      id: 6,
-      image: drainClean,
-      text: 'Drain Cleaning',
-    },
-    {
-      id: 7,
-      image: filter,
-      text: 'Water Filters',
-    },
-    {
-      id: 8,
-      image: pipeRepair,
-      text: 'Pipe Repair And Installs',
-    },
-    {
-      id: 9,
-      image: appliance,
-      text: 'Appliance Installation',
-    },
-    // ... rest of the data
-  ];
-  const footerData = {
-    copyWrite: "Copyright ©2023 Company Name",
-    fb:fb,
-    twitter:twitter,
-    insta:insta,
-    linkdIn:linkdIn,
-    youTube:youTube,
-   
-  };
+
   return (
     <div className="App">
       <div>
-      <Header headerData={headerData} />
-    </div>
-    <div>
-      <Hero 
-      title = {heroData.title}
-      buttonText={heroData.buttonText}
+      <Header headerData={headerData} 
+       text={headerData.text}
+       icon1={headerData.icon1}
+       icon2={headerData.icon2}
+       icon3={headerData.icon3}
+       icon4={headerData.icon4}
+       icon5={headerData.icon5}
+       callIcon={headerData.callIcon}
+       phone={headerData.phone}
+       btnText={headerData.btnText}
+       title={headerData.title}
+       discription={headerData.discription}
+       discription2 = {headerData.discription2}
+       discription3 = {headerData.discription3}
+       image={headerData.image}
       />
     </div>
-    <div>
-      <Experts 
-             title={expertsData.title}
-             discription={expertsData.discription}
-            discription2 = {expertsData.discription2}
-            image={expertsData.image}
-            subSectionTitle={expertsData.subSectionTitle}
-            subSectionDiscrp={expertsData.subSectionDiscrp}
-            name={expertsData.name}
-      />
-    </div>
-    <div>
-      <OurComunity 
-      title = {ourComunityData.title}
-      discription = {ourComunityData.discription}
-      />
-    </div>
-    <PlumbingHelp
-      title = {plumbingData.title}
-      discription = {plumbingData.discription}
-      discription2={plumbingData.discription2}
-      image={plumbingData.image}
-      image2={plumbingData.image2}
-      cards={cards}
-
-    />
-    <CallUs
-    title={callUsData.title}
-    discription={callUsData.discription}
-    btnText={callUsData.btnText}
-    image={callUsData.image}
-    powerText={callUsData.powerText}
-    icon1={callUsData.icon1}
-    icon2={callUsData.icon2}
-    icon3={callUsData.icon3}
-    icon4={callUsData.icon4}
-    licence={callUsData.licence}
-    mail={callUsData.mail}
-    location={callUsData.location}
-    phone={callUsData.phone}
-    servingSan={callUsData.servingSan}
-    ServingSanDisc1={callUsData.ServingSanDisc1}
-    ServingSanDisc2={callUsData.ServingSanDisc2}
-    />
-
-    <Footer 
-     copyWrite={footerData.copyWrite}
-     fb={footerData.fb}
-     insta={footerData.insta}
-     linkdIn={footerData.linkdIn}
-     youTube={footerData.youTube}
-     twitter={footerData.twitter}
-    />
+  
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,133 @@
-import React from 'react'
-const Header = ({ headerData }) => {
+import React from "react";
+const Header = ({
+  headerData,
+  text,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  callIcon,
+  phone,
+  btnText,
+  title,
+  discription,
+  discription2,
+  discription3,
+  image,
+}) => {
   return (
-    <div className=' '>
-       
-       <div className='bg-[#00205B]  '>
-       <div className='container md:max-w-container px-4 lg:px-[112px] min-h-[72px] mx-auto md:flex py-3 md:py-0 justify-between'>
-        <div>
-        <h1 className='text-[#FEFDFD] font-bold text-[20px] leading-[125%] pt-[7px]'>
-        24 Hour Emergency Service
-        </h1>
-        <p className='text-[#FEFDFD] pt-1 font-thin'>Hours: M-F 8:00 AM – 4:30 PM / 1705 W. Lewis St. San Diego, CA 92103</p>
-        </div>
-       <div>
-       <button className='text-[#FEFDFD] border border-[#FEFDFD]  rounded-[2px] mt-1.5 py-[16px] px-[20px]'>Call Now +1 (619) 295-2115</button>
-
-       </div>
-
-       </div>
-       </div>
-
-  <div className='container md:max-w-container px-4 lg:px-[112px]  mx-auto'>
-    <div className="flex justify-between items-center">
-      <div>
-        <img src={headerData.imageUrl} alt="Logo" className="w-[300px] mt-[16px] h-[181px]" />
-      </div>
-      <div className="ml-4 hidden md:block">
-        <div className="flex space-x-4 text-[#00205B]">
-          {headerData.texts.map((text, index) => (
-            <h1  key={index} className='text-blue-900 font-bold'>{text}</h1>
-          ))}
+    <div className=" ">
+      <div className="bg-[#AE7029] ">
+        <div className="container md:max-w-container px-4 lg:px-[112px]  mx-auto min-h-[37px] ">
+          <div>
+            <p className="text-[#FEFDFD] pt-[6px] font-[14px] 2xl:font-[17px] font-thin text-center">
+              Shop all your favorite products and gift cards 24/7 on our online
+              store!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+      <div className="background">
+        <div className="container xl:max-w-container px-4 lg:px-[112px]  mx-auto">
+          <div className="flex justify-between py-[55px]">
+            <div className="grid lg:grid-cols-2 gap-[16px] items-baseline">
+              <div>
+                <h1 className="text-[#AE7029] font-[400] text-[14px] 2xl:text-[17px]">
+                  {text}
+                </h1>
+              </div>
+              <div className="flex gap-[8px] ">
+                <img
+                  src={icon1}
+                  alt="none"
+                  className="h-[16px] w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+                <img
+                  src={icon2}
+                  alt="none"
+                  className="h-[16px] w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+                <img
+                  src={icon3}
+                  alt="none"
+                  className="h-[16px] w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+                <img
+                  src={icon4}
+                  alt="none"
+                  className="h-[16px] w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+                <img
+                  src={icon5}
+                  alt="none"
+                  className="h-[16px] w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col sm:flex-row gap-[24px] ">
+              <div className="flex max-w-[130px] 2xl:max-w-[200px] gap-[4px] lg:mt-4 ">
+                <img
+                  src={callIcon}
+                  alt="none"
+                  className="h-[16px] mt-1 w-[16px] 2xl:h-[20px] 2xl:w-[20px]"
+                />
+                <span className="text-[#AE7029] text-[14px]  2xl:text-[17px] font-[600]">
+                  {phone}
+                </span>
+              </div>
+              <div>
+                <button className="py-[12px] px-[24px] font-[600] border border-[#AE7029] rounded-[4px] text-[#AE7029]">
+                  {btnText}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <div>
+              <img
+                src={headerData.imageUrl}
+                alt="Logo"
+                className="w-[322px] mt-[16px] h-[90px]"
+              />
+            </div>
+            <div className="pt-8 pb-[96px]">
+              <div className="flex flex-wrap justify-center  gap-[48px] text-[#AE7029]">
+                {headerData.texts.map((text, index) => (
+                  <h1 key={index} className="text-[#AE7029] font-bold">
+                    {text}
+                  </h1>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap xl:flex-nowrap px-[112px] justify-center gap-[32px]">
+          <div className="  ">
+            <img
+              src={image}
+              alt="none"
+              className="w-[592px] h-[480px] max-w-[592px]"
+            />
+          </div>
+          <div className="">
+            <h1 className="text-[48px] font-[300] text-[#221F20]  leading-[125%] ">
+              {title}
+            </h1>
+            <p className=" text-[#221F20;]  pt-[16px] text-[16px] 2xl:text-[20px]  ">
+              {discription}
+            </p>
+            <p className=" text-[#221F20;] pt-[16px]  text-[16px] 2xl:text-[20px]  ">
+              {discription2}
+            </p>
+            <p className=" text-[#221F20;] pt-[16px]  text-[16px] 2xl:text-[20px]">
+              {discription3}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
